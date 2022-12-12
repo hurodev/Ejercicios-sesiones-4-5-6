91ff00e9377d906f53a6a553dc054ca35e0afd46
hurodev <hurodev2@gmail.com>
2022-12-12 15:16:34 -0600

    first commit

--- src/main/java/com/example/Ejercicios/sesiones45/y6/LaptopController.java
+++ src/main/java/com/example/Ejercicios/sesiones45/y6/LaptopController.java
@@ -1,14 +1,20 @@
 package com.example.Ejercicios.sesiones45.y6;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LaptopController {
+
+    private final Logger log = LoggerFactory.getLogger(LaptopController.class);
+
 
     public LaptopRepository laptopRepository;
 
@@ -18,18 +24,81 @@ public class LaptopController {
 
 
     @GetMapping("/laptops")
-    public List<Laptop> findAll (){
+    public List<Laptop> findAll() {
 
         return laptopRepository.findAll();
     }
 
-    @PostMapping("/laptops")
-    public Laptop addLaptop(@RequestBody Laptop laptop){
 
-        return laptopRepository.save(laptop);
+    @GetMapping("/laptop/{id}")
+    @ApiOperation("buscar libro por id")
+    public ResponseEntity<Laptop> findOneById(@ApiParam("CLAVER PRIMARIAID TIPO LONG") @PathVariable Long id) {
+
+        Optional<Laptop> bookOpt = laptopRepository.findById(id);
+
+
+        return bookOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
 
     }
 
 
+    @PostMapping("/laptops")
+    public Laptop addLaptop(@RequestBody Laptop laptop) {
 
+        return laptopRepository.save(laptop);
+    }
+
+    @PutMapping("laptops")
+    public ResponseEntity<Object> update(@RequestBody Laptop laptop) {
+
+        if (laptop.getId() == null) {
+            log.warn("error al modificar libro");
+            return ResponseEntity.badRequest().build();
+
+        }
+        if (!laptopRepository.existsById(laptop.getId())) {
+            log.warn("NO SE ENCONTROL EL LIBRO QUE QUIERE MODIDIFCAR libro");
+            return ResponseEntity.notFound().build();
+
+        }
+        Laptop result = laptopRepository.save(laptop);
+        log.info("MODIFICADO");
+
+        return ResponseEntity.ok(result);
+
+
+    }
+
+    @DeleteMapping("laptops")
+    public ResponseEntity<Laptop> delete(@RequestBody Laptop laptop) {
+
+
+        if (laptop.getId() == null) {
+            log.warn("libro no encontrado");
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        if (!laptopRepository.existsById(laptop.getId())) {
+            log.warn("la laptop que deseas eliminar no esta creado");
+            return ResponseEntity.notFound().build();
+        }
+
+
+        laptopRepository.deleteById(laptop.getId());
+        log.info("la laptop eliminada");
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping("laptops/all")
+    public ResponseEntity<Laptop> deleteAll() {
+        log.info("se ejecuto deleleAll");
+        laptopRepository.deleteAll();
+
+        return ResponseEntity.noContent().build();
+
+    }
 }
---
+++ src/test/java/com/example/Ejercicios/sesiones45/y6/LaptopControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.Ejercicios.sesiones45.y6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void addLaptop() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
